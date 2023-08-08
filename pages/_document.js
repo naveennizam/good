f5627a7c31561013e41c55c795849575a87c6ec3
Naveen Nizam <82910319+naveennizam@users.noreply.github.com>
2023-08-08 16:14:36 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,15 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head
-          {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
-      // eslint-disable-next-line @next/next/no-sync-scripts
+      <Head>
+          
+         // eslint-disable-next-line @next/next/no-sync-scripts
             <script
               data-project-id="hoc6lY87EIPCgE6f3mkBHUXqhupXbbxDqsqj7VTJ"
               src="https://snippet.meticulous.ai/v1/meticulous.js" async 
             />
-          )}
-      />
+          
+      < / Head>
  
       <body>
         <Main />
