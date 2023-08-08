09bb635465b52b067ab56532cf9bdbbb7b5e7ce5
Naveen Nizam <82910319+naveennizam@users.noreply.github.com>
2023-08-08 16:41:53 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+
 
 export default function Document() {
   return (
     <Html>
       <Head>
           
-               // eslint-disable-next-line @next/next/no-sync-scripts
-            <Script>
-              data-project-id="hoc6lY87EIPCgE6f3mkBHUXqhupXbbxDqsqj7VTJ"
-              src="https://snippet.meticulous.ai/v1/meticulous.js" async 
-            </Script>
-          
+      <script
+      data-project-id="hoc6lY87EIPCgE6f3mkBHUXqhupXbbxDqsqj7VTJ"
+      src="https://snippet.meticulous.ai/v1/meticulous.js" async
+      />
       < / Head>
  
       <body>
