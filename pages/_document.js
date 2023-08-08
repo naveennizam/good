ffe84398f486395045898d53f98c9af0933b534c
Naveen Nizam <82910319+naveennizam@users.noreply.github.com>
2023-08-08 13:54:58 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
-      <Head>
-          {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
-     
-            <script
-              data-project-id="hoc6lY87EIPCgE6f3mkBHUXqhupXbbxDqsqj7VTJ"
-              src="https://snippet.meticulous.ai/v1/meticulous.js"
-            />
-          )}
-      </Head>
+    <Html lang="en">
+      <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
