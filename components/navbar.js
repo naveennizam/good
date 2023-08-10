bf133962b7da35b162a0b41ea78d88873d5fec4b
Naveen Nizam <naveennizam01@gmail.com>
2023-08-10 10:51:34 +0500

    update

--- components/navbar.js
+++ components/navbar.js
@@ -5,13 +5,14 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <>
-    <nav className={styles.mainnav}>
+      <nav className={styles.mainnav}>
         <ul>
           <Link href={'/'}> <li>Home</li> </Link>
           <Link href={'/about'}> <li>About</li> </Link>
           <Link href={'/blog'}> <li>Blog</li> </Link>
           <li> <Link href={'/contact'}> Contact </Link> </li>
-         
+          <li> <Link href={'/support'}> Support </Link> </li>
+
         </ul>
       </nav>
     </>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import Navbar from '../components/navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
   return( 
